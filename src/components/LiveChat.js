5d983ab9d8bb1330768646f2dcd1fb95c5994bdd
div-rathore <divyanshurathore11@gmail.com>
2023-12-28 23:59:43 +0530

    optimized live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+
+    const[liveMessage, setLiveMessage] = useState('')
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages);
@@ -17,21 +19,38 @@ const LiveChat = () => {
       dispatch(
         addMessage({
           name: generateRandomName(),
-          message: makeRandomMessage(20)
+          message: makeRandomMessage(20),
         })
       );
-    }, 2000);
+    }, 1500);
     return () => {
       clearInterval(i);
     };
   }, []);
 
   return (
+    <>
     <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-      {chatMessages && chatMessages.map((c,index) => (
-        <ChatMessage key={index} name={c.name} message={c.message} />
-      ))}
+      <div>
+        {chatMessages &&
+          chatMessages.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+      </div>
+      
     </div>
+    <form className="w-full p-2 ml-2 border border-black" onSubmit={(e)=> {
+        e.preventDefault()
+        dispatch(addMessage({
+            name:"Divyanshu",
+            message:liveMessage
+        }))
+        setLiveMessage('')
+    }}>
+        <input type="text" className=" px-2 w-9/12" value={liveMessage} onChange={(e)=> setLiveMessage(e.target.value)}/>
+        <button type="submit" className="px-2 mx-2 bg-green-200 rounded rounded-lg">Send</button>
+    </form>
+    </>
   );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyCu82RIDu-Ges2BnX-ZUvp_Wo9c06_-7do";
 
+export const OFFSET_LIVE_CHAT = 25;
+
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 
